818618dc97487cea4038946c496d05302e2e1ef9
Christopher Bernard <ctbernardabc@gmail.com>
2023-09-06 19:41:29 -0400

    Delete src directory

--- src/view/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import listReducer from './groceryListReducer';
-import timerReducer from './timerReducer'
-import totalReducer from './totalReducer';
-
-const rootReducer = combineReducers({ timerReducer, listReducer, totalReducer });
-
-export const Store = createStore(rootReducer, applyMiddleware(thunk)); 
--- src/view/redux/totalActions.js
+++
@@ -1,8 +0,0 @@
-export const SET_TOTAL = 'SET_TOTAL'
-
-export const setTotal = total => dispatch => {
-    dispatch({
-        type: SET_TOTAL,
-        payload: total
-    })
-}
